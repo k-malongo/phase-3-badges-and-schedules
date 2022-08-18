868cca0040bfa92cb898ac7be802834d33c797ee
Kelvin <vokemalosh32@gmail.com>
2022-08-19 01:34:13 +0300

    commit tree 4da5db12bf779c0f2da4a8ca55b1be4a800e529a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+    end
+    
+    def batch_badge_creator array
+        array.collect do |name|
+            "Hello, my name is #{name}."
+        end
+    end
+    
+    def assign_rooms array
+        array.collect.with_index do |name, number|
+            "Hello, #{name}! You'll be assigned to room #{number + 1}!"
+        end
+    end
+    
+    def printer(array)
+        batch_badge_creator(array).each do |name|
+          puts name
+        end
+      
+        assign_rooms(array).each do |room|
+          puts room
+        end
+      end
